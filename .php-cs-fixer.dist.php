97304f428cd18132ec9b377d5974836236482f10
Mathis <handymathis@gmail.com>
2022-09-25 20:14:13 +0200

    [TASK] Ignore vendor

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,9 @@
 <?php
 $config = new PhpCsFixer\Config();
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__);
 return $config->setRules([
 '@PSR12' => true,
 '@Symfony' => true,
-]);
+])->setFinder($finder);
