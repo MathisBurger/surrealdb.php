7adcb70dacb08437a231ada2963b35fa119ca900
Mathis <handymathis@gmail.com>
2022-09-25 21:31:52 +0200

    [TASK] Added phpunit support

--- example/UseDatabase.php
+++ example/UseDatabase.php
@@ -4,6 +4,6 @@ include './vendor/autoload.php';
 
 use MathisBurger\SurrealDb\SurrealDriver;
 
-$conn = new SurrealDriver('ws://surrealdb:8000/rpc');
+$conn = new SurrealDriver('ws://127.0.0.1:8000/rpc');
 $conn->login('root', 'root');
 $conn->useDatabase('test', 'test');
---
+++ tests/ActionTests.php
@@ -0,0 +1,14 @@
+<?php
+
+use MathisBurger\SurrealDb\SurrealDriver;
+use PHPUnit\Framework\TestCase;
+
+class ActionTests extends TestCase
+{
+    public function testConnect(): void
+    {
+        $conn = new SurrealDriver('ws://surrealdb:8000/rpc');
+        $conn->login('root', 'root');
+    }
+
+}
