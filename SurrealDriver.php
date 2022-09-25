6952a49dc5a337438ce1c1de196c872965c361dc
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-09-25 19:36:17 +0200

    [TASK] Added all methods to driver file

--- SurrealDriver.php
+++ SurrealDriver.php
@@ -2,9 +2,9 @@
 
 namespace MathisBurger\SurrealDb;
 
+use Exception;
 use WebSocket\BadOpcodeException;
 use WebSocket\Client;
-use WebSocket\Message\Message;
 
 /**
  * The main file of the surreal db driver
@@ -20,17 +20,78 @@ class SurrealDriver
         $this->socket = new Client($url);
     }
 
-    public function login(string $username, string $password): void {
-        $this->send("signin", ['user' => $username, 'pass' => $password]);
+    public function login(string $username, string $password): void
+    {
+        $this->send('signin', [['user' => $username, 'pass' => $password]]);
     }
 
-    public function useDatabase(string $namespace, string $database): array {
-        return $this->send('use', ['ns' => $namespace, 'db' => $database]);
+    public function useDatabase(string $namespace, string $database): void
+    {
+        $this->send('use', [$namespace, $database]);
+    }
+
+    public function create(string $thing, array $data): mixed
+    {
+        return $this->send('create', [$thing, $data])['result'];
+    }
+
+    public function select(string $what): mixed
+    {
+        return $this->send('select', [$what])['result'];
+    }
+
+    public function info(): mixed
+    {
+        return $this->send('info', [])['result'];
+    }
+
+    public function invalidate(): mixed
+    {
+        return $this->send('invalidate', [])['result'];
+    }
+
+    public function live(string $table): mixed
+    {
+        return $this->send('live', [$table])['result'];
+    }
+
+    public function kill(string $query): mixed
+    {
+        return $this->send('kill', [$query])['result'];
+    }
+
+    public function let(string $key, array $val): mixed
+    {
+        return $this->send('let', [$key, $val])['result'];
+    }
+
+    public function query(string $sql, array $vars): mixed
+    {
+        return $this->send('query', [$sql, $vars])['result'];
+    }
+
+    public function update(string $what, array $data): mixed
+    {
+        return $this->send('update', [$what, $data])['result'];
+    }
+
+    public function change(string $what, mixed $data): mixed
+    {
+        return $this->send('change', [$what, $data]);
+    }
+
+    public function delete(string $what): mixed
+    {
+        return $this->send('delete', [$what])['result'];
     }
 
     /**
+     *
+     * @param string $method
+     * @param array $params
+     * @return array
      * @throws BadOpcodeException
-     * @throws \Exception
+     * @throws Exception
      */
     private function send(string $method, array $params): array
     {
@@ -38,14 +99,13 @@ class SurrealDriver
             'id' => strval(random_int(0, PHP_INT_MAX)),
             'async' => false,
             'method' =>  $method,
-            'params' => [$params]
+            'params' => $params
         ];
         $content = json_encode($request);
         $this->socket->send($content);
         while (true) {
             $response = $this->socket->receive();
             if ($response !== null) {
-                echo $response;
                 return json_decode($response, true);
             }
         }
--- example/Connect.php
+++ example/Connect.php
@@ -6,3 +6,5 @@ use MathisBurger\SurrealDb\SurrealDriver;
 $conn = new SurrealDriver("ws://127.0.0.1:8000/rpc");
 $conn->login("root", "root");
 $conn->useDatabase("test", "test");
+$conn->create("user", ['username' => 'Mathis', 'password' => 'Test']);
+$conn->select("user")['result'];
