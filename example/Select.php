d4dcf62d4b0608e80ee6e37bd2ac7c74815e38ef
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-09-25 19:50:10 +0200

    [TASK] Updated examples

--- example/Select.php
+++ example/Select.php
@@ -5,4 +5,6 @@ use MathisBurger\SurrealDb\SurrealDriver;
 
 $conn = new SurrealDriver("ws://127.0.0.1:8000/rpc");
 $conn->login("root", "root");
-
+$conn->useDatabase("test", "test");
+$conn->create("user", ['username' => 'Mathis', 'password' => 'Test']);
+$conn->select("user")['result'];
--- example/UseDatabase.php
+++ example/UseDatabase.php
@@ -5,4 +5,4 @@ use MathisBurger\SurrealDb\SurrealDriver;
 
 $conn = new SurrealDriver("ws://127.0.0.1:8000/rpc");
 $conn->login("root", "root");
-
+$conn->useDatabase("test", "test");
